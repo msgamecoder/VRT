6dd5ffffad067d2701a31482415664ddcee20d53
msgamecoder <olamilekandamilaraa@gmail.com>
2025-03-15 12:30:32 +0100

    vrt

---
+++ pair.js
@@ -0,0 +1,150 @@
+const PastebinAPI = require('pastebin-js'),
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+const {makeid} = require('./id');
+const express = require('express');
+const fs = require('fs');
+let router = express.Router()
+const pino = require("pino");
+const {
+    default: Gifted_Tech,
+    useMultiFileAuthState,
+    delay,
+    makeCacheableSignalKeyStore,
+    Browsers
+} = require("maher-zubair-baileys");
+
+function removeFile(FilePath){
+    if(!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true })
+ };
+router.get('/', async (req, res) => {
+    const id = makeid();
+    let num = req.query.number;
+        async function GIFTED_MD_PAIR_CODE() {
+        const {
+            state,
+            saveCreds
+        } = await useMultiFileAuthState('./temp/'+id)
+     try {
+            let Pair_Code_By_Gifted_Tech = Gifted_Tech({
+                auth: {
+                    creds: state.creds,
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
+                },
+                printQRInTerminal: false,
+                logger: pino({level: "fatal"}).child({level: "fatal"}),
+                browser: ["Chrome (Linux)", "20.0.4", "VRT"]
+             });`
+    💀 *_The Realm of Pain Welcomes You...* 💀
+    ═════════════════════════════════════
+    
+    🤖 *_Connected by the Great MX-GΔMΞCØDΞR_* 🤖
+    
+    🌩️ *Vortex Rebirth - Designed by the Dark Lord MX-GΔMΞCØDΞR* 🌩️
+
+    ╔════════════════════════════════╗
+    ║ 🔥 *『 Welcome to the REAL DARKNESS! 』* 🔥
+    ║
+    ║ You're foolish enough to make it here. Congrats, I guess.  
+    ║ Let's see if you can survive *VRT*.  
+    ╚════════════════════════════════╝
+    
+    ⚡ *『 BOT REPO - Don’t Dare to Fork Me! 』* ⚡
+    🔗 https://github.com/msgamecoder/vortex-rebirth
+
+    ═════════════════════════════════════
+
+    📢 *『 Join the Cult – WhatsApp Channel 』* 📢
+    🔗 https://chat.whatsapp.com/CB3O8v4T6ho14mDO9TpBYZ
+    
+    ═════════════════════════════════════
+
+    👥 *『 Your Last Chance - WhatsApp Group 』* 👥
+    🔗 https://whatsapp.com/channel/0029Vb06pAP4IBhMMnZtnM3X
+
+    ═════════════════════════════════════
+
+    ⚠️ *_2025 - The Dark Reign of MX-GΔMΞCØDΞR_* ⚠️
+    
+    🤡 *You’re weak if you don’t give my repo a ⭐* 
+    `
+             if(!Pair_Code_By_Gifted_Tech.authState.creds.registered) {
+                await delay(1500);
+                        num = num.replace(/[^0-9]/g,'');
+                            const code = await Pair_Code_By_Gifted_Tech.requestPairingCode(num)
+                 if(!res.headersSent){
+                 await res.send({code});
+                     }
+                 }
+            Pair_Code_By_Gifted_Tech.ev.on('creds.update', saveCreds)
+            Pair_Code_By_Gifted_Tech.ev.on("connection.update", async (s) => {
+                const {
+                    connection,
+                    lastDisconnect
+                } = s;
+                if (connection == "open") {
+                await delay(5000);
+                let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+                await delay(800);
+               let b64data = Buffer.from(data).toString('base64');
+               let session = await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id, { text: '' + b64data });
+
+               let GIFTED_MD_TEXT = `
+🎮 *_Pair Code Connected by MX-GΔMΞCØDΞR_* 🎮
+  
+💻 *_Made With ❤️ by MX-GΔMΞCØDΞR_* 💻  
+───────────────────────────────────────  
+
+╔════════════════════════════════╗  
+║ 🌟 *『 WOW! WELCOME TO THE SAD REALM 』* 🌟
+║  
+║ You’ve successfully completed the first step to deploy *MX-1.0*.  
+║
+╚════════════════════════════════╝  
+
+📂 *『 BOT REPO 』* 📂  
+
+🔗 https://github.com/themxgamecoder/MX-1.0 
+
+───────────────────────────────────────  
+
+📢 *『 WHATSAPP CHANNEL 』* 📢  
+
+🔗 https://chat.whatsapp.com/CB3O8v4T6ho14mDO9TpBYZ
+
+───────────────────────────────────────  
+
+👥 *『 WHATSAPP GROUP 』* 👥  
+
+🔗 https://whatsapp.com/channel/0029Vavz0e6E50Ugp30Z6z0W  
+
+
+───────────────────────────────────────  
+
+✨ *2025 - MX-GΔMΞCØDΞR* ✨  
+
+💡 _Don’t forget to give a ⭐ to my repo!_  
+`
+
+ await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id,{text: GIFTED_MD_TEXT},{quoted:session})
+ 
+
+        await delay(100);
+        await Pair_Code_By_Gifted_Tech.ws.close();
+        return await removeFile('./temp/'+id);
+            } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(10000);
+                    GIFTED_MD_PAIR_CODE();
+                }
+            });
+        } catch (err) {
+            console.log("service restated");
+            await removeFile('./temp/'+id);
+         if(!res.headersSent){
+            await res.send({code:"Service Unavailable"});
+         }
+        }
+    }
+    return await GIFTED_MD_PAIR_CODE()
+});
+module.exports = router
